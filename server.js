f4de69b1006243aa36a31ccb059aeb0befb0ec59
Zhuruijin0331 <zhuruijin0331@icloud.com>
2026-03-23 16:19:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 这行是关键！让express自动读取public文件夹
+// 托管静态文件
 app.use(express.static('public'));
 
+// 强制访问首页
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// 启动服务
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log('Server started');
 });
